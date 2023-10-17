b87d3d7ccbac369be1508a015daaebc65634c167
unknown <CalebVoila>
2023-10-17 17:26:55 +0530

    updating the files

--- src/calebnav.js
+++ src/calebnav.js
@@ -5,10 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
         dropdown.classList.toggle('hidden');
     }
 
+    // Close all dropdowns
+    function closeAllDropdowns() {
+        document.querySelectorAll('[data-dropdown-toggle]').forEach(function (toggle) {
+            const dropdownId = toggle.getAttribute('data-dropdown-toggle');
+            const dropdown = document.getElementById(dropdownId);
+            dropdown.classList.add('hidden');
+        });
+    }
+
     // Toggle dropdowns on click
     document.querySelectorAll('[data-dropdown-toggle]').forEach(function (toggle) {
         toggle.addEventListener('click', function () {
             const dropdownId = this.getAttribute('data-dropdown-toggle');
+            closeAllDropdowns(); // Close all dropdowns before opening a new one
             toggleDropdown(dropdownId);
         });
     });
@@ -16,13 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Close dropdowns when clicking outside
     document.addEventListener('click', function (event) {
         if (!event.target.closest('[data-dropdown-toggle]')) {
-            document.querySelectorAll('[data-dropdown-toggle]').forEach(function (toggle) {
-                const dropdownId = toggle.getAttribute('data-dropdown-toggle');
-                const dropdown = document.getElementById(dropdownId);
-                if (!dropdown.contains(event.target)) {
-                    dropdown.classList.add('hidden');
-                }
-            });
+            closeAllDropdowns();
         }
     });
 });
