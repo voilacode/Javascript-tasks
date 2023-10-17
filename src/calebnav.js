da04957749889c73218870d98b209dc776099a47
unknown <CalebVoila>
2023-10-17 23:21:16 +0530

    updating files

--- src/calebnav.js
+++ src/calebnav.js
@@ -5,28 +5,45 @@ document.addEventListener('DOMContentLoaded', function () {
         dropdown.classList.toggle('hidden');
     }
 
-    // Close all dropdowns
-    function closeAllDropdowns() {
-        document.querySelectorAll('[data-dropdown-toggle]').forEach(function (toggle) {
-            const dropdownId = toggle.getAttribute('data-dropdown-toggle');
-            const dropdown = document.getElementById(dropdownId);
-            dropdown.classList.add('hidden');
-        });
-    }
-
-    // Toggle dropdowns on click
+    // Toggle dropdowns on click or touchstart
     document.querySelectorAll('[data-dropdown-toggle]').forEach(function (toggle) {
-        toggle.addEventListener('click', function () {
+        toggle.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the default link behavior
             const dropdownId = this.getAttribute('data-dropdown-toggle');
-            closeAllDropdowns(); // Close all dropdowns before opening a new one
+            toggleDropdown(dropdownId);
+        });
+
+        // For mobile devices
+        toggle.addEventListener('touchstart', function (event) {
+            event.preventDefault(); // Prevent the default link behavior
+            const dropdownId = this.getAttribute('data-dropdown-toggle');
+            toggleDropdown(dropdownId);
+        });
+
+        // For desktop devices
+        toggle.addEventListener('mouseenter', function (event) {
+            event.preventDefault(); // Prevent the default link behavior
+            const dropdownId = this.getAttribute('data-dropdown-toggle');
+            toggleDropdown(dropdownId);
+        });
+
+        // For desktop devices - close dropdown on mouseleave
+        toggle.addEventListener('mouseleave', function (event) {
+            event.preventDefault();
+            const dropdownId = this.getAttribute('data-dropdown-toggle');
             toggleDropdown(dropdownId);
         });
     });
 
-    // Close dropdowns when clicking outside
-    document.addEventListener('click', function (event) {
-        if (!event.target.closest('[data-dropdown-toggle]')) {
-            closeAllDropdowns();
+    // Close dropdowns when clicking or touching outside
+    document.addEventListener('click', closeDropdownsOutside);
+    document.addEventListener('touchstart', closeDropdownsOutside);
+
+    function closeDropdownsOutside(event) {
+        if (!event.target.closest('[data-dropdown-toggle]') && !event.target.closest('.group')) {
+            document.querySelectorAll('.dropdown').forEach(function (dropdown) {
+                dropdown.classList.add('hidden');
+            });
         }
-    });
+    }
 });
